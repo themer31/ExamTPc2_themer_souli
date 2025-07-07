6f32e880d67ab7e3f9e69bb656df2ae854325cd9
themer31 <themersouli31@gmail.com>
2025-07-07 20:23:19 +0100

    13

--- projet.py
+++ projet.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def moyenne(T)
     S=0
     for t in T:
@@ -9,6 +10,8 @@ Moy=statistics.mean(Data)
 print("La moyenne est :", Moy)
 
 =======
+=======
+>>>>>>> f61d41d (13)
 <<<<<<< HEAD
 Data=[1,3,5]
 Moy=statistics.mean(Data)
@@ -20,4 +23,16 @@ def moyenne(T)
         S++=T
         return S
 >>>>>>> 7d879d7 (commiitt)
+<<<<<<< HEAD
 >>>>>>> 59bf824 (commiitt)
+=======
+=======
+Data=[1,3,5]
+if Data :
+print('la moyenne est :',Moy(Data))
+print('le min est :',min(Data))
+print('le max est :',max(Data))
+else :
+    print('la liste est vide')
+>>>>>>> e8587ec (13)
+>>>>>>> f61d41d (13)
